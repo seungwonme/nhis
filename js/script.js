76b41ec71b8a1a58c848c1abb1e681b230c8461a
seungwonme <seungwonan.me@gmail.com>
2022-12-21 18:38:52 +0900

    repair gnb with jquery and complete width 767 mediaquery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-document.querySelector("#layerPopup button").addEventListener("click", function(){
+document.addEventListener('DOMContentLoaded',function(){
+    document.querySelector("#layerPopup button").addEventListener("click", function(){
     document.querySelector("#layerPopup").style.display="none";
 })
+});
+
+
+
+$(function(){
+    $(".gnbMenu, .topMenu").mouseover(function () {
+        $(".gnbMenu").stop().slideDown(150);
+    });
+    $(".gnbMenu, .topMenu").mouseleave(function () {
+        $(".gnbMenu").stop().slideUp(150);
+    });
+});
