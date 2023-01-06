b1993a0e14b4d325e72d94b98219b2df29ca8d4f
seungwonme <seungwonan.me@gmail.com>
2023-01-06 16:02:26 +0900

    complete stupid banner

--- js/script.js
+++ js/script.js
@@ -37,8 +37,7 @@ const noticeBannerNext = document.querySelector('.boxRight');
 const noticeBannerPrev = document.querySelector('.boxLeft');
 const noticeBannerStop = document.querySelector('.boxStop');
 const noticeBannerImg = document.querySelector('.bgImg');
-const noticeBannerBox = document.querySelectorAll('.boxViewport .cf li').length;
-const nth = document.querySelector('.nth');
+const nth = document.querySelector('.noticeBanner em');
 let i = 1;
 let index = 1;
 
@@ -109,10 +108,16 @@ noticeBannerStop.addEventListener('click', function () {
     noticeBannerStop.classList.toggle('off');
 });
 
-document.querySelector('.boxControlDirection').addEventListener('mouseenter', function () {
+document.querySelector('.noticeBanner .boxLeft').addEventListener('mouseenter', function () {
     clearInterval(id);
 });
-document.querySelector('.boxControlDirection').addEventListener('mouseleave', function () {
+document.querySelector('.noticeBanner .boxLeft').addEventListener('mouseleave', function () {
+    autoBanner();
+});
+document.querySelector('.noticeBanner .boxRight').addEventListener('mouseenter', function () {
+    clearInterval(id);
+});
+document.querySelector('.noticeBanner .boxRight').addEventListener('mouseleave', function () {
     autoBanner();
 });
 noticeBanner.addEventListener('mouseenter', function () {
@@ -122,3 +127,98 @@ noticeBanner.addEventListener('mouseleave', function () {
     autoBanner();
 });
 //~notice banner
+
+const promotionBanner = document.querySelector('.promotionZone .boxViewport');
+const promotionBannerNext = document.querySelector('.promotionZone .boxRight');
+const promotionBannerPrev = document.querySelector('.promotionZone .boxLeft');
+const promotionBannerStop = document.querySelector('.promotionZone .boxStop');
+const promotionBannerImg = document.querySelector('.promotionZone img');
+const nth2 = document.querySelector('.promotionZone em');
+let j = 1;
+let index2 = 1;
+
+function next2() {
+    if (index2 === 3) {
+        index2 = 0;
+    }
+    index2++;
+    j++;
+    nth2.innerText = index2;
+    promotionBanner.style.transform = `translate(-${j * promotionBannerImg.width}px)`;
+    if (j === 1) {
+        --index2;
+        promotionBanner.classList.remove('on');
+        next2();
+        promotionBanner.classList.add('on');
+    } else if (j === 4) {
+        j = 0;
+    }
+}
+function prev2() {
+    if (index2 === 1) {
+        index2 = 4;
+    }
+    index2--;
+    j--;
+    nth2.innerText = index2;
+    promotionBanner.style.transform = `translate(-${j * promotionBannerImg.width}px)`;
+    if (j === 3) {
+        ++index2;
+        promotionBanner.classList.remove('on');
+        prev2();
+        promotionBanner.classList.add('on');
+    } else if (j === 0) {
+        j = 4;
+    }
+}
+promotionBannerNext.addEventListener('click', next2);
+promotionBannerPrev.addEventListener('click', prev2);
+
+const autoBanner2 = function () {
+    id2 = setInterval(function () {
+        if (index2 === 3) {
+            index2 = 0;
+        }
+        index2++;
+        j++;
+        nth2.innerText = index2;
+        promotionBanner.style.transform = `translate(-${j * promotionBannerImg.width}px)`;
+        if (j === 1) {
+            --index2;
+            promotionBanner.classList.remove('on');
+            next2();
+            promotionBanner.classList.add('on');
+        } else if (j === 4) {
+            j = 0;
+        }
+    }, 3000);
+};
+autoBanner2();
+
+promotionBannerStop.addEventListener('click', function () {
+    if (promotionBannerStop.classList[1] === undefined) {
+        clearInterval(id2);
+    } else {
+        autoBanner2();
+    }
+    promotionBannerStop.classList.toggle('off');
+});
+
+document.querySelector('.promotionZone .boxLeft').addEventListener('mouseenter', function () {
+    clearInterval(id2);
+});
+document.querySelector('.promotionZone .boxLeft').addEventListener('mouseleave', function () {
+    autoBanner2();
+});
+document.querySelector('.promotionZone .boxRight').addEventListener('mouseenter', function () {
+    clearInterval(id2);
+});
+document.querySelector('.promotionZone .boxRight').addEventListener('mouseleave', function () {
+    autoBanner2();
+});
+promotionBanner.addEventListener('mouseenter', function () {
+    clearInterval(id2);
+});
+promotionBanner.addEventListener('mouseleave', function () {
+    autoBanner2();
+}); 
